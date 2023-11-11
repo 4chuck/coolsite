958a7cd1cf679a6182c57a1f5743700b8da8c1ae
tony <130490654+4chuck@users.noreply.github.com>
2023-11-11 17:10:50 +0530

    Add files via upload

---
+++ login/log(database).js
@@ -0,0 +1,42 @@
+function store(){
+
+    var name = document.getElementById('name');
+    var pw = document.getElementById('pw');
+
+    if(name.value.length == 0){
+        alert('Please fill in name');
+
+    }
+      else{
+        localStorage.setItem('name', name.value);
+        localStorage.setItem('pw', pw.value);
+        alert('Your account has been created');
+    }
+}
+
+function check(){
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+}
+
+function copyToClipboard(elementId) {
+alert("your password " + storedPw + " copied");
+
+  // Create a "hidden" input
+  var aux = document.createElement("input");
+
+  // Assign it the value of the specified element
+  aux.setAttribute("value",document.getElementById(elementId).innerHTML);
+
+  // Append it to the body
+  document.body.appendChild(aux);
+
+  // Highlight its content
+  aux.select();
+
+  // Copy the highlighted text
+  document.execCommand("copy");
+
+  // Remove it from the body
+  document.body.removeChild(aux); 
+}
