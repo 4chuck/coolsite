298d8524ce8c136c906135f35f0cfab6a2e388a0
tony <130490654+4chuck@users.noreply.github.com>
2023-11-22 16:58:22 +0530

    Update search.js

--- search.js
+++ search.js
@@ -3,15 +3,15 @@ function opener() {
     var targetNames = {
   
         blog : "https://be-marvel.blogspot.com",
-        dog : "http://4chuck.github.io/coolsite/DOG.html",
-        cat : "https://4chuck.github.io/coolsite/cat.html",
-        ironman :"https://4chuck.github.io/coolsite/iron%20man.html",
-        nature :"https://4chuck.github.io/coolsite/sea.html",
-        go :"https://4chuck.github.io/coolsite/go.html",
-       blackboard :"https://4chuck.github.io/coolsite/black%20board.html",
-        search :"https://4chuck.github.io/coolsite/search.html",
-        calci :"https://4chuck.github.io/coolsite/calci.html",
-        home :"https://4chuck.github.io/coolsite"
+        dog : "DOG.html",
+        cat : "cat.html",
+        ironman :"iron%20man.html",
+        nature :"sea.html",
+        go :"go.html",
+       blackboard :"black%20board.html",
+        search :"search.html",
+        calci :"calci.html",
+        home :"index.html"
        
     };
     if (text in targetNames) {
