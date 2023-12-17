3c93463253aa9035ef8ae418fa3dad2a77ada6bb
tony <130490654+4chuck@users.noreply.github.com>
2023-12-17 10:56:33 +0530

    Update log.js

--- login/log.js
+++ login/log.js
@@ -10,7 +10,7 @@ function store(){
       else{
         localStorage.setItem('name', name.value);
         localStorage.setItem('pw', pw.value);
-        alert('success</br>redirecting to home page');
+        alert('success 😊 redirecting to home page');
     }
 }
 
