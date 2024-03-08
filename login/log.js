5d3ad3a50557e8e70f9ccaf1abbf18dc33622b49
tony <130490654+4chuck@users.noreply.github.com>
2024-03-08 16:14:48 +0530

    Update log.js

--- login/log.js
+++ login/log.js
@@ -22,5 +22,21 @@ function check() {
 function copyToClipboard(pw) {
     check(); // Retrieve decrypted password before copying
     alert("Your password: " + storedPw + " copied");
-    // Copy to clipboard 
+    // Create a "hidden" input
+  var aux = document.createElement("input");
+
+  // Assign it the value of the specified element
+  aux.setAttribute("value",document.getElementById(elementId).innerHTML);
+
+  // Append it to the body
+  document.body.appendChild(aux);
+
+  // Highlight its content
+  aux.select();
+
+  // Copy the highlighted text
+  document.execCommand("copy");
+
+  // Remove it from the body
+  document.body.removeChild(aux); 
   }
